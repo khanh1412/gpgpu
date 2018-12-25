5711bc5e59895f837d7d0ff29888bba36c9c11c4
Nguyen Ngoc Khanh <khanh@localhost.localdomain>
2018-12-25 15:25:55 +0700

    modified:   Makefile 	modified:   windows_make.bat

--- Makefile
+++ Makefile
@@ -10,8 +10,8 @@ lib:
 	$(CC) $(CC_FLAGS) $(INCLUDE) -c -fPIC -o objects/Kernel.o sources/Kernel.cc
 	$(CC) $(CC_FLAGS) $(INCLUDE) -c -fPIC -o objects/Queue.o sources/Queue.cc
 	$(CC) $(CC_FLAGS) $(INCLUDE) -c -fPIC -o objects/Context.o sources/Context.cc
-	$(CC) $(CC_FLAGS) $(INCLUDE) -shared -o libCLWrapper.so objects/*.o $(LIBRARY)
+	$(CC) $(CC_FLAGS) $(INCLUDE) -shared -o libCL.so objects/*.o
 	rm -rf objects
 
 test: lib
-	$(CC) $(CC_FLAGS) $(INCLUDE) -o run examples/test.cc ./libCLWrapper.so $(LIBRARY)
+	$(CC) $(CC_FLAGS) $(INCLUDE) -o run examples/test.cc ./libCL.so $(LIBRARY)
--- windows_make.bat
+++ windows_make.bat
@@ -4,11 +4,11 @@ g++ -std=c++17 -I.\include -c -fPIC -o objects\Kernel.o sources\Kernel.cc -w
 g++ -std=c++17 -I.\include -c -fPIC -o objects\Queue.o sources\Queue.cc   -w
 g++ -std=c++17 -I.\include -c -fPIC -o objects\Context.o sources\Context.cc -w
 
-g++ -std=c++17 -I.\include -shared -o CLWrapper.dll objects\*.o .\OpenCL.dll -w
+g++ -std=c++17 -I.\include -shared -o CL.dll objects\*.o .\OpenCL.dll -w
 
 del objects\*.o
 rmdir objects
 
-g++ -std=c++17 -I.\include -o run.exe examples\test.cc .\ClWrapper.dll .\OpenCL.dll -w -lpthread
+g++ -std=c++17 -I.\include -o run.exe examples\test.cc .\Cl.dll .\OpenCL.dll -w -lpthread
 
 

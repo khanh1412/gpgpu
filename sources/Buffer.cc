762cd1379af985acbf9dfe80ac8c10d0486e850c
Nguyen Ngoc Khanh <khanh@localhost.localdomain>
2018-12-25 22:36:56 +0700

    stable

--- sources/Buffer.cc
+++ sources/Buffer.cc
@@ -1,6 +1,7 @@
 #include"CL.h"
 using namespace CL;
-Buffer::Buffer(const cl_context& context, cl_mem_flags flags, size_t size){
+Buffer::Buffer(const cl_context& context, cl_mem_flags flags, size_t size)
+{
 	buffer = clCreateBuffer(context, flags, size, nullptr, nullptr);
 }
 Buffer::~Buffer()

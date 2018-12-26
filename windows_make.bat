1e3f77ab1389d98f50d42164fdbde17f23ea0128
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2018-12-26 12:14:51 +0800

    remove cl*.h

--- windows_make.bat
+++ windows_make.bat
@@ -1,14 +1,15 @@
 mkdir objects
-g++ -std=c++17 -I.\include -c -fPIC -o objects\Buffer.o sources\Buffer.cc -w
-g++ -std=c++17 -I.\include -c -fPIC -o objects\Kernel.o sources\Kernel.cc -w
-g++ -std=c++17 -I.\include -c -fPIC -o objects\Queue.o sources\Queue.cc   -w
-g++ -std=c++17 -I.\include -c -fPIC -o objects\Context.o sources\Context.cc -w
+g++ -std=c++17 -I.\include -c -fPIC -o objects\Buffer.o sources\Buffer.cc
+g++ -std=c++17 -I.\include -c -fPIC -o objects\Kernel.o sources\Kernel.cc
+g++ -std=c++17 -I.\include -c -fPIC -o objects\Queue.o sources\Queue.cc
+g++ -std=c++17 -I.\include -c -fPIC -o objects\Context.o sources\Context.cc
+g++ -std=c++17 -I.\include -c -fPIC -o objects\Debugger.o sources\Debugger.cc
 
-g++ -std=c++17 -I.\include -shared -o CL.dll objects\*.o .\OpenCL.dll -w
+g++ -std=c++17 -I.\include -shared -o CL.dll objects\*.o .\OpenCL.dll
 
 del objects\*.o
 rmdir objects
 
-g++ -std=c++17 -I.\include -o run.exe examples\test.cc .\Cl.dll .\OpenCL.dll -w -lpthread
+g++ -std=c++17 -I.\include -o run.exe examples\test.cc .\Cl.dll .\OpenCL.dll -lpthread
 
 

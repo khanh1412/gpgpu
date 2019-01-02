1db06461ce8cc54abba0b8ca6c1b5e49c3eb4f0f
Nguyen Ngoc Khanh <khanh@localhost.localdomain>
2019-01-02 23:02:14 +0700

    DONE WITH EVENTS

--- examples/1_event/main.cc
+++ examples/1_event/main.cc
@@ -11,11 +11,12 @@ void SWAP(float *a0, float *a1, uint64_t COUNT)
 	auto q1 = context.createQueue();
 	auto q0 = context.createQueue();
 
-	auto write = q0.writeBuffer(d, a0, COUNT*sizeof(float));
-	//q0.synchronize();
-	//write.wait();
-	q1.waitForEventsWithBarrier({write});
+	auto control = context.createUserEvent();
+	q1.waitForEventsWithBarrier({control});
 	auto read = q1.readBuffer(d, a1, COUNT*sizeof(float));
+	auto write = q0.writeBuffer(d, a0, COUNT*sizeof(float));
+	write.wait();
+	control.setCompleted();
 	
 
 	q1.synchronize();

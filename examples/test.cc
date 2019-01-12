7b2428f5ef3140100b87a5483a6ee74d35a738a2
Nguyen Ngoc Khanh <khanh@localhost.localdomain>
2019-01-13 00:57:06 +0800

    modified:   examples/test.cc

--- examples/test.cc
+++ examples/test.cc
@@ -1,6 +1,13 @@
 #include"Context.h"
 #include<iostream>
+#include"Handler.h"
 int main()
 {
+	{
+		auto h1 = Handler::initHandler(SIGABRT);
+		auto h2 = Handler::initHandler(SIGSEGV);
+
+		*(int*) 0 = 0;
+	}
  	return 0;	
 }

1f975aa9782115c0cbea937dadfc06d3f35a8a61
Nguyen Ngoc Khanh <khanh@localhost.localdomain>
2019-01-13 00:34:09 +0800

    modified:   examples/test.cc

--- examples/test.cc
+++ examples/test.cc
@@ -1,17 +1,20 @@
 #include"Context.h"
 #include<iostream>
 #include"Handler.h"
+#include<thread>
 void func()
 {
+	while (true)
 	*(int*) 0 = 0;
 }
 
 int main()
 {
 	{
-		auto h = Handler::initHandler(SIGSEGV);
-		func();
+		auto h1 = Handler::initHandler(SIGSEGV);
+		std::thread t(func); t.detach();
+		h1.wait();
+		std::cout<<"Done!"<<std::endl;
 	}
-		
  	return 0;	
 }

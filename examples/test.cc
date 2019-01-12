65abe0d266013244448643e98ad2dd78ebe3dd37
Nguyen Ngoc Khanh <khanh@localhost.localdomain>
2019-01-12 20:18:07 +0800

    modified:   examples/test.cc 	new file:   include/Handler.h

--- examples/test.cc
+++ examples/test.cc
@@ -1,34 +1,12 @@
 #include"Context.h"
-
-#include <iostream>
-#include<string.h>
-#include <signal.h>
-
-
-void handler(int nSignum, siginfo_t* si, void* vcontext)
+#include<iostream>
+#include"Handler.h"
+int main()
 {
-  
-	ucontext_t* context = (ucontext_t*)vcontext;
-	context->uc_mcontext.gregs[REG_RIP]++;
-	std::cout << "Segmentation fault" << std::endl;
-	exit(1);
-}
-
-
-int main() {
-	std::cout << "Start" << std::endl;
-  
-	struct sigaction action;
-	memset(&action, 0, sizeof(struct sigaction));
-	action.sa_flags = SA_SIGINFO;
-	action.sa_sigaction = handler;
-	sigaction(SIGSEGV, &action, NULL);
-  
-//	int* x = 0;
-//	int y = *x;
-	auto contexts = Context::initContexts();
-  
-	std::cout << "End" << std::endl;
-  
-	return 0;
+	enable_handler();
+		int *x = 0;
+		int y = *x;
+	disable_handler();
+ 	return 0; 
+	
 }
---
+++ include/Handler.h
@@ -0,0 +1,34 @@
+#ifndef _HANDLER_H_
+#define _HANDLER_H_
+#include<signal.h>
+#include<string.h>
+#include<stdio.h>
+#include<stdexcept>
+static void ignore_handler(int signum, siginfo_t *si, void *context)
+{
+	((ucontext_t*)context)->uc_mcontext.gregs[REG_RIP]++;
+	fprintf(stderr, "Segmentation fault ignored\n");
+}
+static void exit_handler(int signum, siginfo_t *si, void *context)
+{
+	((ucontext_t*)context)->uc_mcontext.gregs[REG_RIP]++;
+	fprintf(stderr, "Segmentation fault handled\n");
+	exit(1);
+}
+void enable_handler()
+{
+        struct sigaction action;
+        memset(&action, 0, sizeof(struct sigaction));
+        action.sa_flags = SA_SIGINFO;
+        action.sa_sigaction = ignore_handler;
+        sigaction(SIGSEGV, &action, NULL);	
+}
+void disable_handler()
+{
+        struct sigaction action;
+        memset(&action, 0, sizeof(struct sigaction));
+        action.sa_flags = SA_SIGINFO;
+        action.sa_sigaction = exit_handler;
+        sigaction(SIGSEGV, &action, NULL);	
+}
+#endif

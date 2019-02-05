cb844846482eb15f9be5d67a6cad93c5214b28c2
Nguyen Ngoc Khanh <khanh@localhost.localdomain>
2019-02-05 18:20:28 +0800

    ADD PLATFORM INFO

--- examples/test.cc
+++ examples/test.cc
@@ -15,5 +15,11 @@ int main()
 		std::cout<<err.what()<<std::endl;
 	}
 	auto all_platforms = cl::platform::get_all_platforms();
+	auto& platform = all_platforms[0];
+	std::cout<<platform.profile()<<std::endl;
+	std::cout<<platform.version()<<std::endl;
+	std::cout<<platform.name()<<std::endl;
+	std::cout<<platform.vendor()<<std::endl;
+	std::cout<<platform.extensions()<<std::endl;
 	return 0;
 }
--- include/CL/opencl/platform.h
+++ include/CL/opencl/platform.h
@@ -14,6 +14,12 @@ class platform: public singleton
 	public:
 		~platform() {}
 		static container<platform> get_all_platforms();
+	public:
+		std::string profile();
+		std::string version();
+		std::string name();
+		std::string vendor();
+		std::string extensions();
 };
 container<platform> platform::get_all_platforms()
 {
@@ -29,5 +35,45 @@ container<platform> platform::get_all_platforms()
 platform::platform(const cl_platform_id& platform_id)
 	: handler(platform_id)
 {}
+std::string platform::profile()
+{
+	size_t size;
+	cl_assert(clGetPlatformInfo(handler, CL_PLATFORM_PROFILE, 0, nullptr, &size));
+	array<char> string(size+1); string.data()[size] = '\0';
+	cl_assert(clGetPlatformInfo(handler, CL_PLATFORM_PROFILE, size, string.data(), nullptr));
+	return std::string(string.data(), string.size());
+}
+std::string platform::version()
+{
+	size_t size;
+	cl_assert(clGetPlatformInfo(handler, CL_PLATFORM_VERSION, 0, nullptr, &size));
+	array<char> string(size+1); string.data()[size] = '\0';
+	cl_assert(clGetPlatformInfo(handler, CL_PLATFORM_VERSION, size, string.data(), nullptr));
+	return std::string(string.data(), string.size());
+}
+std::string platform::name()
+{
+	size_t size;
+	cl_assert(clGetPlatformInfo(handler, CL_PLATFORM_NAME, 0, nullptr, &size));
+	array<char> string(size+1); string.data()[size] = '\0';
+	cl_assert(clGetPlatformInfo(handler, CL_PLATFORM_NAME, size, string.data(), nullptr));
+	return std::string(string.data(), string.size());
+}
+std::string platform::vendor()
+{
+	size_t size;
+	cl_assert(clGetPlatformInfo(handler, CL_PLATFORM_VENDOR, 0, nullptr, &size));
+	array<char> string(size+1); string.data()[size] = '\0';
+	cl_assert(clGetPlatformInfo(handler, CL_PLATFORM_VENDOR, size, string.data(), nullptr));
+	return std::string(string.data(), string.size());
+}
+std::string platform::extensions()
+{
+	size_t size;
+	cl_assert(clGetPlatformInfo(handler, CL_PLATFORM_EXTENSIONS, 0, nullptr, &size));
+	array<char> string(size+1); string.data()[size] = '\0';
+	cl_assert(clGetPlatformInfo(handler, CL_PLATFORM_EXTENSIONS, size, string.data(), nullptr));
+	return std::string(string.data(), string.size());
+}
 }
 #endif

06a80ef24a8bce667916c8e829b72b6dbbf3b48c
Nguyen Ngoc Khanh <khanh@localhost.localdomain>
2019-02-06 13:38:19 +0800

    deleted:    examples/3_rsa/key.h 	modified:   examples/3_rsa/keypair.h

--- examples/3_rsa/keypair.h
+++ examples/3_rsa/keypair.h
@@ -1,6 +1,38 @@
 #ifndef _KEYPAIR_H_
 #define _KEYPAIR_H_
-#include"key.h"
+#include<iostream>
+#include<gmpxx.h>
+using int_t = mpz_class;
+class key
+{
+	public:
+		key(){}
+		key(const int_t& value, const int_t& modulo) : value(value), modulo(modulo) {}
+		key& operator=(const key& obj) {value = obj.value; modulo = obj.modulo; return *this;}
+		int_t value;
+		int_t modulo;
+	public:
+		~key() {}
+		int_t code(const int_t& message) const;
+		inline friend std::ostream& operator<<(std::ostream& out, const key& k);
+};
+inline std::ostream& operator<<(std::ostream& out, const key& k)
+{
+	out<<"key: "<<std::endl;
+	out<<"\tvalue : "<<k.value<<std::endl;
+	out<<"\tmodulo: "<<k.modulo<<std::endl;
+	return out;
+}
+static inline int_t crypto_raise(const int_t& a, const int_t& n, const int_t& modulo)
+{
+	int_t pow;
+	mpz_powm_sec(pow.get_mpz_t(), a.get_mpz_t(), n.get_mpz_t(), modulo.get_mpz_t());
+	return pow;
+}
+int_t key::code(const int_t& message) const
+{
+	return crypto_raise(message, value, modulo); 
+}
 class keypair
 {
 	public:
@@ -29,14 +61,11 @@ static inline int_t fast_inverse(const int_t& num, const int_t& modulo)
 keypair::keypair(int_t p, int_t q, const int_t& pubkey)
 {
 	setnextprime(p);
-
 	setnextprime(q);
 
 	int_t n = p*q;
 	int_t phi = (p-1)*(q-1);
-
 	int_t e = pubkey;
-
 	int_t d = fast_inverse(e, phi);
 
 	pub = key(e, n);

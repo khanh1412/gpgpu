5626cba87f289ad0f9cfd325994eb8275dd13b35
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2019-02-10 00:32:54 +0800

    modified:   include/CL/builtin/gemm.h

--- include/CL/builtin/gemm.h
+++ include/CL/builtin/gemm.h
@@ -1,7 +1,6 @@
 #ifndef _GEMM_H_
 #define _GEMM_H_
 #include"CL/opencl/queue.h"
-#include<clblast_c.h>
 #include<clblast.h>
 namespace cl {
 namespace builtin {
@@ -12,14 +11,6 @@ event gemm(queue& q, size_t m, size_t n, size_t k, type alpha, type beta, const 
 	size_t b_ld = n;
 	size_t c_ld = n;
 	cl_event e;
-	/*
-	CLBlastTranspose transposeA = CLBlastTransposeNo;
-	CLBlastTranspose transposeB = CLBlastTransposeNo;
-	cl_assert(CLBlastSgemm(CLBlastLayoutRowMajor, transposeA, transposeB, 
-				m, n, k, 
-				alpha, A.handler, a_offset, a_ld, B.handler, b_offset, b_ld, 
-				beta, C.handler, c_offset, c_ld, &q.handler, &e));
-	*/
 	clblast::Transpose transposeA = clblast::Transpose::kNo;
 	clblast::Transpose transposeB = clblast::Transpose::kNo;
 	cl_assert((cl_int)clblast::Gemm<type>(clblast::Layout::kRowMajor,

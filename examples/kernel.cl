c01596d02c93da771b7a6ba9baee0c28c7119013
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2019-02-09 20:01:27 +0800

    deleted:    examples/kernel.cl 	modified:   examples/test.cc

--- examples/kernel.cl
+++
@@ -1,7 +0,0 @@
-__kernel void add(	__global float* z,
-			__global const float* x,
-			__global const float* y)
-{
-	ulong i = get_global_id(0);
-	z[i] = x[i] + y[i];
-}
--- examples/test.cc
+++ examples/test.cc
@@ -1,30 +1,5 @@
 #include<iostream>
-#include"CL/opencl_lazy.h"
 int main(int argc, char **argv)
 {
-	const size_t COUNT = 3;
-	const size_t size = COUNT*sizeof(float);
-
-	float x[3] = {1,2,3};
-	float y[3] = {3,2,1};
-	float z[3];
-
-	chooseDevice(1);
-	auto kernel = createKernel("./examples/kernel.cl");
-	auto bx = createBuffer(size);
-	auto by = createBuffer(size);
-	auto bz = createBuffer(size);
-
-	std::cout<<"write x: "<<enqueueWriteBuffer(bx, x, size)<<" ns"<<std::endl;
-	std::cout<<"write y: "<<enqueueWriteBuffer(by, y, size)<<" ns"<<std::endl;
-	std::cout<<"kernel : "<<enqueueExecuteKernel(kernel, {bz, bx, by}, {3})<<" ns"<<std::endl;
-	std::cout<<"read z : "<<enqueueReadBuffer(bz, z, size)<<" ns"<<std::endl;
-
-	std::cout<<z[0]<<z[1]<<z[2]<<std::endl;
-
-	std::cout<<"Device name: "<<device->name()<<std::endl;
-	std::cout<<"Device version: "<<device->version()<<std::endl;
-	
-
 	return 0;
 }

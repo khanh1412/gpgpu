0538b23a5ae9ba61f52f0ce4f03fa0efe7c0f191
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2019-02-21 23:43:35 +0800

    modified:   examples/test.cc

--- examples/test.cc
+++ examples/test.cc
@@ -5,7 +5,21 @@ int main(int argc, char **argv)
 	auto all_devices = cl::device::get_all_devices();
 	auto& device = all_devices[1];
 	std::cout<<device.name()<<std::endl;
+	std::cout<<device.name()<<std::endl;
+	std::cout<<device.name()<<std::endl;
+	std::cout<<device.name()<<std::endl;
+	std::cout<<device.name()<<std::endl;
+	std::cout<<device.name()<<std::endl;
+	std::cout<<device.name()<<std::endl;
+	std::cout<<device.name()<<std::endl;
+	std::cout<<device.name()<<std::endl;
+	std::cout<<device.name()<<std::endl;
+	std::cout<<device.name()<<std::endl;
+	std::cout<<device.name()<<std::endl;
+	std::cout<<device.name()<<std::endl;
+	std::cout<<device.name()<<std::endl;
 	auto context = cl::context({device});
 	std::cout<<device.name()<<std::endl;
+	std::cout<<"test ok"<<std::endl;
 	return 0;
 }
